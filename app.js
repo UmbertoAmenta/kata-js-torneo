3aad6882030cf02e1321d3472b8b77bbff4dbb89
Umberto Amenta <umberto.amenta@hotmail.it>
2025-09-07 20:39:51 +0200

    refact: improve console messages

--- app.js
+++ app.js
@@ -1,18 +1,43 @@
 import { fighters, weapons } from "./database.js";
 
-console.log("🐉 Che inizi il torneo! 🐉");
+console.log("🐉 Che il torneo abbia inizio! 🐉");
+console.log("");
+
 // -------------------------------------------------
 // FASE 0 - preparazione del torneo
 
 // Iscrizioni al torneo
-console.log("🤼 Combattenti iscritti", fighters);
 const registeredFighters = [...fighters];
 
 // Armi a disposizione dei combattenti
 const availableWeapons = [...weapons];
 
+// Se le armi non bastano, aggiunge la possibilità di combattere a mani nude
+while (availableWeapons.length < registeredFighters.length) {
+  const noWeapon = { name: "bare hands", power: 0 };
+  availableWeapons.push(noWeapon);
+}
+
+// DATI
+// console.log("🤼 Combattenti iscritti", fighters);
+// console.log("⚔️ Armi disponibili", weapons);
+
+// ANNUNCI
+const fightersNames = registeredFighters.map((f) => f.name);
+const weaponsNames = availableWeapons.map((w) => w.name);
+
+console.log(
+  "Ecco a voi gli sfidanti..",
+  fightersNames,
+  "..e le armi a loro disposizione!",
+  weaponsNames
+);
+console.log("");
+
 // -------------------------------------------------
 // FASE 1 - 🔥 Scelta dell'arma
+
+console.log("🔥 Fase 1 - Scelta dell'arma");
 
 // Modifica della struttura dati
 const currentFighters = registeredFighters.map((f) => ({
@@ -41,13 +66,6 @@ function fisherYatesShuffle(array) {
 
 const shuffledFighters = fisherYatesShuffle(currentFighters);
 
-// Se le armi non bastano, aggiunge la possibilità di combattere a mani nude
-while (availableWeapons.length < shuffledFighters.length) {
-  const noWeapon = { name: "bare hands", power: 0 };
-  availableWeapons.push(noWeapon);
-}
-console.log("⚔️ Armi disponibili", weapons);
-
 // Assegnazione casuale delle armi
 const armedFighters = shuffledFighters.map((f) => {
   const randomWeaponId = Math.floor(Math.random() * availableWeapons.length);
@@ -61,12 +79,25 @@ const armedFighters = shuffledFighters.map((f) => {
   };
 });
 
-// Combattenti armati
-console.log("🔥 Fase 1 - Scelta dell'arma");
-console.log("Combattenti armati", armedFighters);
+// DATI
+// console.log("Combattenti armati", armedFighters);
+
+// ANNUNCI
+const armedFightersNames = armedFighters.map((f) => [
+  f.fighter.name,
+  f.weapon.name,
+]);
+
+console.log(
+  "Curiosi di conoscere gli oggetti ottenuti dai combattenti?",
+  armedFightersNames
+);
+console.log("");
 
 // -------------------------------------------------
 // FASE 2 - 💪 Allenamento
+
+console.log("💪 Fase 2 - Allenamento");
 
 // Generazione casuale del moltiplicatore dovuto all'allenamento
 const trainedFighters = armedFighters.map((f) => {
@@ -79,23 +110,36 @@ const trainedFighters = armedFighters.map((f) => {
   };
 });
 
-console.log("💪 Fase 2 - Allenamento");
-console.log("Allenamento completato", trainedFighters);
+// DATI
+// console.log("Allenamento completato", trainedFighters);
+
+// ANNUNCI
+console.log("Avranno usufruito tutti della stanza dello spirito e del tempo?");
+console.log("");
 
 // -------------------------------------------------
 // FASE 3 - 🎯 Qualificazione:
 
-const qualifiedFighters = trainedFighters.filter((f) => f.totalPower >= 2000);
-
 console.log("🎯 FASE 3 - Qualificazione");
 
-console.log(
-  "Ecco a voi i Guerrieri qualificati agli incontri!",
-  qualifiedFighters
-);
+const qualifiedFighters = trainedFighters.filter((f) => f.totalPower >= 2000);
+
+// DATI
+// console.log(
+//   "Ecco a voi i Guerrieri qualificati agli incontri!",
+//   qualifiedFighters
+// );
+
+// ANNUNCI
+const qualifiedFightersNames = qualifiedFighters.map((f) => f.fighter.name);
+
+console.log("Chi avrà superato le qualificazioni?", qualifiedFightersNames);
+console.log("");
 
 // -------------------------------------------------
 // FASE 4 - ⚔️ Combattimento
+
+console.log("⚔️ Fase 4 - Combattimento");
 
 const readyToFight = structuredClone(qualifiedFighters);
 
@@ -109,9 +153,16 @@ if (qualifiedFighters.length % 2 == 1) {
 
   readyToFight.splice(robotIndex, 0, robot);
 
+  // DATI
+  // console.log(
+  //   "Come da regolamento, un nuovo sfidante si aggiunge al tabellone affinchè tutti i partecipanti abbiano un avversario!!",
+  //   readyToFight
+  // );
+
+  // ANNUNCI
   console.log(
     "Come da regolamento, un nuovo sfidante si aggiunge al tabellone affinchè tutti i partecipanti abbiano un avversario!!",
-    readyToFight
+    "Diamo il benvenuto al Red Ribbon Robot e ringraziamo la Red Ribbon, sponsor ufficiale del torneo!!"
   );
 }
 
@@ -137,18 +188,31 @@ while (toClashes.length > 0) {
   matches.push(match);
 }
 
-console.log("⚔️ Fase 4 - Combattimento");
+// DATI
+// console.log(
+//   "Ecco le coppie di sfidanti e i vincitori degli incontri!!",
+//   matches
+// );
+
+// ANNUNCI
+const matchesNames = matches.map((m) => {
+  return `${m.fighters[0].fighter.name} VS ${m.fighters[1].fighter.name}`;
+});
+const matchesWinnersNames = matches.map((match) => match.winner.fighter.name);
+
+console.log("Ecco a voi le coppie di sfidanti!!", matchesNames);
 console.log(
-  "Ecco le coppie di sfidanti e i vincitori degli incontri!!",
-  matches
+  "Gli incontri sono stati vinti rispettivamente da:",
+  matchesWinnersNames
 );
+console.log("");
 
 // -------------------------------------------------
 // FASE 5 - 🏆 Premiazione
 
-const winners = matches.map((match) => {
-  return match.winner;
-});
+console.log("🏆 Fase 5 - Premiazione");
+
+const winners = matches.map((match) => match.winner);
 
 const podium = winners
   .sort((a, b) => b.totalPower - a.totalPower)
@@ -156,5 +220,10 @@ const podium = winners
 
 // console.log(winners);
 
-console.log("🏆 Fase 5 - Premiazione");
+// DATI
 console.log("Ecco a voi i vincitori!!", podium);
+
+// ANNUNCI
+console.log(
+  `Al terzo posto ${podium[2].fighter.name} preceduto da ${podium[1].fighter.name}. In testa alla classifica, il torneo BoolKaichi viene vinto da... ${podium[0].fighter.name} con un livello di potenza pari a ${podium[0].totalPower}!!`
+);
